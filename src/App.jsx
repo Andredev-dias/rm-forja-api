19466ee16f9d32d65d03d43513aacfd1f8df6b37
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-12-01 10:25:05 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,10 @@ function App() {
     <>
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
-        <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
-        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem'/>
+        <div style={{display: "flex",flexWrap: "wrap", gap: "5px", alignItems: "center", justifyContent: "center"}}>
+          <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
+          <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um nome'/>
+        </div>
      
         <div className={s.wrapCards}>
           {data.map((item, index) => {
